b8ef7fbaeaddd2940890cda61398f6bb78ad8174
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-29 11:32:12 -0500

    Replace URL with port number in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -80,7 +80,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run start',
-        url: 'http://localhost:3000',
+        port: 3000,
         reuseExistingServer: !process.env.CI,
     },
 });
