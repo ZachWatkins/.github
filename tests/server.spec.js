72223eb5dc0a6e158077fd42fdc0d038a53bc5b3
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-02 22:22:20 -0500

    Test the new server file

---
+++ tests/server.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+import { start, stop } from '../scripts/server.js'
+
+test('server can start and stop', async () => {
+    const server = await start(3000, true)
+    expect(server.listening).toBe(true)
+    await stop(server)
+    expect(server.listening).toBe(false)
+})
