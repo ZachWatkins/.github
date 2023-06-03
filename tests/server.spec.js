0433913c38bbae03c86c421a0db5d5cc353a6092
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-03 07:18:12 -0500

    Change the port number for the file server to avoid errors with the Vite server

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 import { start, stop } from '../scripts/server.js'
 
 test('server can start and stop', async () => {
-    const server = await start(3000, true)
+    const server = await start(3001, true)
     expect(server.listening).toBe(true)
     await stop(server)
     expect(server.listening).toBe(false)
