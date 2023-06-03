3bcfd420b04b8590a756023043e1db1bbe951bd0
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-03 08:17:13 -0500

    Create server test fixture

---
+++ tests/serverTest.js
@@ -0,0 +1,12 @@
+import baseTest from '@playwright/test'
+import { start, stop } from '../scripts/server.js'
+
+const serverTest = baseTest.extend({
+    server: [async ({}, use) => { // eslint-disable-line no-empty-pattern
+        const server = await start(3000, true)
+        await use(server)
+        await stop(server)
+    }, { scope: 'worker' }],
+})
+
+export default serverTest
