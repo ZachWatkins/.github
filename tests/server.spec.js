04d6042174baaf9fa0f499082095f42e4928aca6
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-03 08:17:27 -0500

    Change server test to use fixture

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,9 +1,6 @@
-import { test, expect } from '@playwright/test'
-import { start, stop } from '../scripts/server.js'
+import { expect } from '@playwright/test'
+import test from './serverTest.js'
 
-test('server can start and stop', async () => {
-    const server = await start(3001, true)
+test('server can start and stop', async ({server}) => {
     expect(server.listening).toBe(true)
-    await stop(server)
-    expect(server.listening).toBe(false)
 })
