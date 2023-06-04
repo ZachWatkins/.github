c75750368fa57c5a38ccaf164ac7629bcc841621
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-03 22:23:06 -0500

    Expand test of server script

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test'
 import test from './serverTest.js'
 
-test('server can start and stop', async ({server}) => {
+test('server routes to the repository\'s root folder', async ({server, page}) => {
     expect(server.listening).toBe(true)
+    const response = await page.goto('http://localhost:3001')
+    expect(response.status()).toBe(200)
+    await expect(page).toHaveTitle(/Index/)
 })
