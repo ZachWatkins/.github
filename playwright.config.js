f1628d5895064282744b2a16a9df3a5955ef0799
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-07 22:34:50 -0500

    Remove webserver configuration and the global baseUrl fixture

--- playwright.config.js
+++ playwright.config.js
@@ -32,9 +32,6 @@ export default defineConfig({
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:3000',
-
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
     },
@@ -76,11 +73,4 @@ export default defineConfig({
             use: { channel: 'chrome' },
         },
     ],
-
-    /* Run your local dev server before starting the tests */
-    webServer: {
-        command: 'npm run start',
-        port: 3000,
-        reuseExistingServer: !process.env.CI,
-    },
 })
