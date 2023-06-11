0a77a84376c8f17d7f576b73d80baf2dd2aea836
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-06-11 12:45:52 -0500

    Add more dialogue choices

--- scripts/init/init.mjs
+++ scripts/init/init.mjs
@@ -24,13 +24,67 @@ if (process.argv.indexOf('--help') >= 0) {
 
 const choices = {}
 const options = {
-    name: String,
-    author: String,
-    license: String,
-    contributing: Boolean,
-    js: Boolean,
-    azure: Boolean,
-    ghPages: Boolean,
+    name: {
+        type: String,
+        default: 'new-repository',
+        description: 'The name of the repository.',
+    },
+    description: {
+        type: String,
+        default: 'A new repository',
+        description: 'A description of the repository.',
+    },
+    author: {
+        type: String,
+        default: 'Your Name',
+        description: 'The name of the repository\'s primary author.',
+    },
+    license: {
+        type: String,
+        default: 'None',
+        description: 'The software license to use for the repository, see here for known software licenses: https://spdx.org/licenses/',
+    },
+    public: {
+        type: Boolean,
+        default: false,
+        description: 'Whether the repository should be public or private.',
+        next: {
+            true: {
+                openSource: {
+                    type: Boolean,
+                    default: false,
+                    description: 'Whether the repository should be open source or not.',
+                },
+            },
+        }
+    },
+    purpose: {
+        type: String,
+        default: 'project',
+        description: 'The purpose of the repository. Choose "project" if the repository is intended to be used as-is (website, application, CLI-only program, etc). Choose "library" if the repository is intended to be used within other codebases.',
+        accepts: ['project', 'library'],
+        next: {
+            project: {},
+            library: {},
+        }
+    },
+    team: {
+        type: Boolean,
+        default: false,
+        description: 'Whether the repository is developed by a team.',
+    },
+    js: {
+        type: Boolean,
+        default: true,
+    },
+    ghPages: {
+        type: Boolean,
+        default: false,
+    },
+    azure: {
+        type: Boolean,
+        default: false,
+    },
 }
 
 const prompt = readline.createInterface({
